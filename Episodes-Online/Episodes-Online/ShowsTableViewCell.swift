24f96dc98f2f9050147ddc0fb11c5b48771ace17
Ian <ian@Ians-MacBook-Pro.local>
2019-09-11 07:40:18 -0400

    added coca file for showsTVcell

---
+++ Episodes-Online/Episodes-Online/ShowsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShowsTableViewCell.swift
+//  Episodes-Online
+//
+//  Created by Ian Cervone on 9/11/19.
+//  Copyright © 2019 Ian Cervone. All rights reserved.
+//
+
+import UIKit
+
+class ShowsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
