bf3d9c5a8a20be643a7e034fe079b5e2e10e52b7
Ian <ian@Ians-MacBook-Pro.local>
2019-09-12 10:01:50 -0400

    added UITVdataSource and stubs

--- Episodes-Online/Episodes-Online/ShowsViewController.swift
+++ Episodes-Online/Episodes-Online/ShowsViewController.swift
@@ -20,27 +20,46 @@ class ShowsViewController: UIViewController {
       }
     }
   
-  var searchString: String = "" {
+  var searchString: String = "girls" {
     didSet {
       loadData()
     }
   }
   
   func loadData() {
-  let urlString = "http://api.tvmaze.com/search/shows?q=\(searchString)"
-//    ShowsAPIClient.manager.getShows(from: urlString, completionHandler: {self.shows = $0}, errorHandler: {print($0)})
+  let urlString = "http://api.tvmaze.com/search/shows?q=girls"
+//    ShowsAPIClient.manager.getShows(completionHandler: {self.shows = $0})
   }
 
-  
-  
+
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    loadData()
+    showsTableView.dataSource = self
+    showsTableView.delegate = self
   }
 
 
 }
 
+extension ShowsViewController: UITableViewDelegate {
+}
 
-
+extension ShowsViewController: UITableViewDataSource{
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return shows.count
+  }
+  
+  
+  //  THE SHOWS RATING NEEDS TO BE AN OPTIONAL VALUE, THERE IS SOME CASES WHERE A SHOW HAS NOT YET BEEN RATED.  HOW WILL THIS BE HANDLED
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let showResults = shows[indexPath.row]
+    let cell = showsTableView.dequeueReusableCell(withIdentifier: "showsCell", for: indexPath) as! ShowsTableViewCell
+    cell.showTitleLabel?.text = showResults.shows.name
+    cell.showRatingLabel?.text = String(showResults.shows.rating.average)
+    return cell
+  }
+  
+  
+}
