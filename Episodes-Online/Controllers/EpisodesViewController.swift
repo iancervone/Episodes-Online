7c1a39e87a4468ab36225c4463127b9bcc28994f
Ian <ian@Ians-MacBook-Pro.local>
2019-10-12 20:51:17 -0400

    created episodeDetailVC and corresponding cocoaTouch

--- Episodes-Online/Controllers/EpisodesViewController.swift
+++ Episodes-Online/Controllers/EpisodesViewController.swift
@@ -89,7 +89,7 @@ extension EpisodesViewController: UITableViewDelegate, UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    return 130
+    return 110
   }
   
 }
